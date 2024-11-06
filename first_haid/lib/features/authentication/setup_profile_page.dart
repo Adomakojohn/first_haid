f3727b4b5cb8d01cc800456b6abf531827fa6f9c
Adomako John <johnkwakuadomako6@gmail.com>
2024-11-06 12:55:19 +0000

    updated signup ui

---
+++ first_haid/lib/features/authentication/setup_profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SetupProfilePage extends StatefulWidget {
+  const SetupProfilePage({super.key});
+
+  @override
+  State<SetupProfilePage> createState() => _SetupProfilePageState();
+}
+
+class _SetupProfilePageState extends State<SetupProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ first_haid/lib/routes/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const String home = '/home';
+  static const String login = '/login';
+  static const String setupprofile = '/setupprofile';
+  static const String signuppage = '/signuppage';
+  // Add more routes as needed
+}
