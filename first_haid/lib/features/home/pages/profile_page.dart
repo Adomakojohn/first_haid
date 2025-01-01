87005d247328528fd0aae85ebf5b83e2f6354f64
Adomakojohn <johnkwakuadomako6@gmail.com>
2025-01-01 15:14:51 +0000

    changes

--- first_haid/lib/features/home/pages/profile_page.dart
+++ first_haid/lib/features/home/pages/profile_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/widgets/setup_profile_widget.dart';
+
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
@@ -10,12 +12,27 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final screenWidth = screenSize.width;
+    final screenHeight = screenSize.height;
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: const Text(
           "Profile",
           style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: screenHeight * 0.25,
+              width: 300,
+              child: const SetupProfileWidget(),
+            ),
+          ],
         ),
       ),
     );
